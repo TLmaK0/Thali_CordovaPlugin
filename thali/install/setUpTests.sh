9c54b0f4123aab5a2dad94c53ee6b0ee21a8764b
Ville Rantala <ville.rantala@microsoft.com>
2015-10-20 13:02:01 +0300

    TBD

--- thali/install/setUpTests.sh
+++ thali/install/setUpTests.sh
@@ -15,7 +15,7 @@ mkdir -p ThaliTest/thaliDontCheckIn/localdev
 cp -r Thali_CordovaPlugin/test/www/ ThaliTest/www
 cd ThaliTest/www/jxcore
 cordova platform add ios
-cordova platform add android
+#cordova platform add android
 jx npm install ../../../Thali_CordovaPlugin/thali --save --autoremove "*.gz"
 # SuperTest which is used by some of the BVTs include a PEM file (for private keys) that makes Android unhappy
 # so we remove it below in addition to the gz files.
@@ -25,6 +25,6 @@ jx npm install --autoremove "*.gz,*.pem"
 find . -name "*.gz" -delete
 find . -name "*.pem" -delete
 cp -v $1 app.js
-cordova build android --release --device
-# cordova build ios --device
+#cordova build android --release --device
+cordova build ios --device
 echo "Remember to start the test coordination server by running jx index.js"
